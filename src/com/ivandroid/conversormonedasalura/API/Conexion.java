cdbb2602d344a39f69e9111fb41806624ad92204
IvanCS719 <ivancupil17@gmail.com>
2024-06-09 12:51:01 -0600

    Creando la clase conexion.java

---
+++ src/com/ivandroid/conversormonedasalura/API/Conexion.java
@@ -0,0 +1,24 @@
+package com.ivandroid.conversormonedasalura.API;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Conexion {
+    public static String conectarAPI(String enlaceBase){
+        try {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(enlaceBase))
+                .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error al procesar el API: " + e.getMessage());
+        }
+        return "{API sin datos}";
+    }
+}
