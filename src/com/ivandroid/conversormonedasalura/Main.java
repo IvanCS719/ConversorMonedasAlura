7bd3c3e8cc5096b5c1e7c25ed37722aeef009a3c
IvanCS719 <ivancupil17@gmail.com>
2024-06-09 12:51:20 -0600

    agregando el metodo main

--- src/com/ivandroid/conversormonedasalura/Main.java
+++ src/com/ivandroid/conversormonedasalura/Main.java
@@ -1,4 +1,9 @@
 package com.ivandroid.conversormonedasalura;
 
+import com.ivandroid.conversormonedasalura.API.Conexion;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(Conexion.conectarAPI("https://open.er-api.com/v6/latest/USD"));
+    }
 }
